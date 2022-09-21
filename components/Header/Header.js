5537cea025641426a509209806911ceef3bd4531
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-09-22 01:43:55 +0600

    make mobile header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,27 +2,32 @@ import React from 'react'
 import classes from './Header.module.css';
 export const Header = () => {
   return (
-    <div className='bg-black text-white flex flex-row justify-between items-center '>
+    <div className='bg-black text-white grid grid-cols-1 items-center content-center '>
         
-        <div className='flex flex-row items-center justify-between'>
+        <div className='grid'>
 
-        <div className='logo ml-4'>
-            <h1 className='text-xl font-bold'>Wasi Web Bangladesh</h1>
+        <div className='logo grid grid-cols-1 ml-4 mb-3'>
+            <h1 className='text-xl font-bold mr-6 flex items-center justify-center'>WW Bangladesh</h1>
+            
+            <div className=' absolute top-0 right-1 '>
+                cart box
+            </div>
         </div>
 
-        <ul className=" flex flex-row mr-5">
-            <li className="py-2 px-3 font-medium hover:bg-red-400 hover:">Home</li>
-            <li className="py-2 px-3 font-medium hover:bg-red-400 hover:">About Us</li>
-            <li className="py-2 px-3 font-medium hover:bg-red-400 hover:">Contact</li>
-            <li className="py-2 px-3 font-medium hover:bg-red-400 hover:">Blogs</li>
+        </div>
+
+        <div className='flex items-center justify-center search-bar text-black mb-4'>
+          <input className='  rounded px-1 focus:outline-none' type="text" placeholder=' search...'/>
+        </div>
+
+        <ul className="grid grid-cols-1 ">
+            <li className="py-2 px-3 font-medium hover:bg-red-400 flex justify-center  items-center  ">Home</li>
+            <li className="py-2 px-3 font-medium hover:bg-red-400 flex justify-center  items-center">About Us</li>
+            <li className="py-2 px-3 font-medium hover:bg-red-400 flex justify-center  items-center">Contact</li>
+            <li className="py-2 px-3 font-medium hover:bg-red-400 flex justify-center  items-center">Blogs</li>
             
         </ul>
-        </div>
 
-
-        <div className='mr-3'>
-            cart box
-        </div>
     </div>
   )
 }
