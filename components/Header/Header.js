fafea3e63fcee7e611ab5e73a8e02424dbd82213
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-09-22 13:27:01 +0600

    nav customization

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
-import classes from "./Header.module.css";
 export const Header = () => {
   return (
-    <div className="w-80 h-96 rounded bg-black text-white grid grid-cols-1 md:grid-cols-3 items-center  ">
-      <div className="relative">
-        <div className=" relative top-0 left-1 ">
-          <div className="absolute top-0 right-1">cart</div>
-          <div className="absolute top-0 left-1">X</div>
+    <div className="w-80 md:w-full h-96 md:h-14 rounded bg-black text-white grid grid-cols-1 md:grid-cols-4 items-center drop-shadow-lg  ">
+      <div className="logo grid grid-cols-1 mb-1">
+        <div className=" relative top-0 left-1 mr-3 ">
+          <div className="absolute top-0 right-1  md:hidden">cart</div>
+          <div className="absolute top-0 left-1 md:hidden">X</div>
         </div>
+        <h1 className="text-xl font-bold flex items-center justify-center">
+          WW Bangladesh
+        </h1>
+      </div>
+
+      <div className="relative">
         <div className="mt-2 flex items-center justify-center search-bar text-black mb-4">
           <input
             className=" rounded px-1 focus:outline-none"
@@ -17,26 +22,25 @@ export const Header = () => {
         </div>
       </div>
 
-      <div className="logo grid grid-cols-1 mb-1">
-        <h1 className="text-xl font-bold flex items-center justify-center">
-          WW Bangladesh
-        </h1>
-      </div>
-
-      <ul className="grid grid-cols-1 ">
-        <li className="py-2 px-3 font-medium hover:bg-red-400 flex justify-center  items-center  ">
+      <ul className="grid grid-cols-1 md:grid-cols-4 ">
+        <li className="py-2 px-3 font-medium hover:bg-slate-800 flex justify-center  items-center cursor-pointer">
           Home
         </li>
-        <li className="py-2 px-3 font-medium hover:bg-red-400 flex justify-center  items-center">
-          About Us
+        <li className="py-2 px-3 font-medium hover:bg-slate-800 flex justify-center  items-center cursor-pointer">
+          About
         </li>
-        <li className="py-2 px-3 font-medium hover:bg-red-400 flex justify-center  items-center">
+        <li className="py-2 px-3 font-medium hover:bg-slate-800 flex justify-center  items-center  cursor-pointer">
           Contact
         </li>
-        <li className="py-2 px-3 font-medium hover:bg-red-400 flex justify-center  items-center">
+        <li className="py-2 px-3 font-medium hover:bg-slate-800 flex justify-center  items-center  cursor-pointer">
           Blogs
         </li>
       </ul>
+      <div className="grid grid-cols-1 mr-4 ">
+        <div className="hidden py-2 px-3 font-medium hover:bg-slate-900 md:flex justify-end  items-center">
+          cart
+        </div>
+      </div>
     </div>
   );
 };
