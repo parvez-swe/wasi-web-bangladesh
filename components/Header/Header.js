6be142dc726319572a4cfdbcbbcb7ba8dea3b20a
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-10-11 11:15:51 +0600

    routes added

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,16 +2,16 @@ import React from "react";
 import { useState } from "react";
 import {AiOutlineMenu} from 'react-icons/ai';
 import {AiOutlineClose} from 'react-icons/ai';
-
+import Link from "next/link";
 export const Header = () => {
-  const [showBar, setShowBar] = useState(false);
+  const [showBar, setShowBar] = useState(true);
   const clickHandler = () => {
     setShowBar(!showBar);
   };
   return (
     <>
-      <div className="nav bg-black text-white text-2xl h-20 flex items-center justify-between relative">
-        <h1>Flower</h1>
+      <div className="nav bg-black text-white text-2xl h-20 flex items-center justify-between relative border-b border-slate-400">
+        <h1 className="cursor-pointer">Flower</h1>
         <div className="toggleIcon md:hidden">
           {showBar ? (
             <button onClick={clickHandler}>
@@ -28,10 +28,22 @@ export const Header = () => {
             !showBar ? "linksMobile" : "linksMobile  linksMobileHideNav"
           } `}
         >
-          <li className="link" onClick={clickHandler}>Home</li>
-          <li className="link" onClick={clickHandler}>Contact</li>
-          <li className="link" onClick={clickHandler}>About</li>
-          <li className="link" onClick={clickHandler}>Blog</li>
+          <li className="link" onClick={clickHandler}>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li className="link" onClick={clickHandler}>
+            Contact
+          </li>
+          <li className="link" onClick={clickHandler}>
+            About
+          </li>
+          <li className="link" onClick={clickHandler}>
+            <Link href="/blogs">
+              <a>Blog</a>
+            </Link>
+          </li>
         </ul>
       </div>
     </>
